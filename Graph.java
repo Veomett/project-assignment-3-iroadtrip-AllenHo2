bddef48e0fe3f6ad42f6394079c022b7dce4890b
AllenHo2 <aho22@dons.usfca.edu>
2023-12-04 13:36:24 -0800

    In the process of conversion

--- Graph.java
+++ Graph.java
@@ -8,149 +8,119 @@ import java.util.*;
 
 import static java.lang.Integer.MAX_VALUE;
 public class Graph {    
-    public class Edge implements Comparable<Edge>{
-        Integer source;
-        Integer dest;
-        int weight;
-        Edge(){
-            source = null;
-            dest = null;
-            weight = 0;
-        }
-        Edge(int v1, int v2, int w){
-            source = v1;
-            dest = v2;
-            weight = w;
-        }
-        public String toString(){
-            //return "source: " + source + " dest " + dest + " weight: " + weight;
-            return source + ":" + dest + "(" + weight + ") ";
-        }
-        public int compareTo(Edge e){// needed for Kruskal's algorithm
-            return this.weight - e.weight;
-        }
+    private HashMap<String, HashMap<String, Integer>> countries;  // Hashmap for countries/nodes
+    private HashMap<String, HashMap<String, Integer>> distances;  // Hashmap for distances between countries
+    private HashMap<String, HashMap<String, Integer>> borders;  
+    private HashMap<String, String> countryIDToCountryName = new HashMap<String, String>();
+
+    public Graph(HashMap<String, HashMap<String, Integer>> countries, HashMap<String, HashMap<String, Integer>> distances, HashMap<String, String> countryIDToCountryName) {
+        this.countries = new HashMap<>();
+        this.distances = new HashMap<>();
+        this.borders = new HashMap<>();
+        this.countryIDToCountryName = new HashMap<>();
     }
 
-    private LinkedList<Edge>[] vertexArr;
-    private int numVertices;
-    // This is used for Dijkstra's algorithm
-    private class NodeCost implements Comparable<NodeCost>{
-        int node;
-        int cost;
-        NodeCost(int n, int c){
-            node=n;
-            cost=c;
-        };
-        @Override
-        public int compareTo(NodeCost nc1)
-        {
-            return this.cost - nc1.cost;
+    public void addCountry(String country) {
+        if (!countries.containsKey(country)) {
+            countries.put(country, new HashMap<>());
+            distances.put(country, new HashMap<>());
+            borders.put(country, new HashMap<>());
         }
     }
+    public void addDistance(String country1, String country2, int distance) {
+        addCountry(country1);
+        addCountry(country2);
 
-    private NodeCost[] nodeCosts;
+        // Add edge with distance
+        distances.get(country1).put(country2, distance);
+        distances.get(country2).put(country1, distance);
+    }
 
-    @SuppressWarnings("unchecked")
-    Graph(int n){ // declares graph with n vertices
-        numVertices = n;
-        vertexArr = (LinkedList<Graph.Edge>[]) new LinkedList<?>[n];
-        nodeCosts = new NodeCost[numVertices];
-        for (int i = 0; i < numVertices; i++){
-            vertexArr[i] = new LinkedList<>();
-            nodeCosts[i] = new NodeCost(i, MAX_VALUE);
+    public void addBorder(String country, String neighbor) {
+        addCountry(country);
+        addCountry(neighbor);
+
+        // Add the neighboring country
+        borders.get(country).put(neighbor, new Integer(neighbor));
+        borders.get(neighbor).put(country, new Integer(0));
+    }
+
+    public List<String> dijkstra(String start, String end) {
+        Map<String, Integer> distancesFromStart = new HashMap<>();
+        Map<String, String> previous = new HashMap<>();
+        PriorityQueue<String> queue = new PriorityQueue<>(Comparator.comparingInt(distancesFromStart::get));
+
+        // Initialize distances and queue
+        for (String country : countries.keySet()) {
+            distancesFromStart.put(country, country.equals(start) ? 0 : Integer.MAX_VALUE);
+            previous.put(country, null);
+            queue.add(country);
         }
-    }
 
-    public void addEdge(int v1, int v2, int weight){
-        Edge v1Edge = new Edge(v1, v2, weight);
-        vertexArr[v1].add(v1Edge);
-        Edge v2Edge = new Edge(v2, v1, weight);
-        vertexArr[v2].add(v2Edge);
-    }
-
-    public void addDirEdge(int v1, int v2, int w){
-        Edge myEdge = new Edge(v1, v2, w);
-        vertexArr[v1].add(myEdge);
-    }
-
-    public void removeEdge(int v1, int v2){
-        LinkedList<Edge> v1LL = vertexArr[v1];
-        Iterator<Edge> v1it = v1LL.iterator();
-        Edge currEdge;
-        while(v1it.hasNext()){
-            currEdge = v1it.next();
-            if(currEdge.source == v1 & currEdge.dest == v2 ) {
-                v1it.remove();
+        while (!queue.isEmpty()) {
+            String current = queue.poll();
+            if (current.equals(end)) {
+                // Found the shortest path to the destination
+                List<String> path = new ArrayList<>();
+                while (previous.get(current) != null) {
+                    path.add(current);
+                    current = previous.get(current);
+                }
+                Collections.reverse(path);
+                return path;
             }
-        }
-        LinkedList<Edge> v2LL = vertexArr[v2];
-        Iterator<Edge> v2it = v2LL.iterator();
-        while(v2it.hasNext()){
-            currEdge = v2it.next();
-            if(currEdge.source == v2 & currEdge.dest == v1 ) {
-                v2it.remove();
-            }
-        }
-    }
 
-    public void incidentEdges(int v1){
-        Iterator<Edge> iterator = vertexArr[v1].iterator();
-        System.out.println("Integers adjacent to " + v1 + " are ");
-        iterator.forEachRemaining(System.out::println);
-    }
-
-    public boolean areAdjacent(int v1, int v2){
-        boolean toReturn = false;
-        LinkedList<Edge> v1LL = vertexArr[v1];
-        Iterator<Edge> v1it = v1LL.iterator();
-        Edge currEdge;
-        while(v1it.hasNext()){
-            currEdge = v1it.next();
-            if(currEdge.source == v1 & currEdge.dest == v2 ) {
-                toReturn = true;
-            }
-        }
-        LinkedList<Edge> v2LL = vertexArr[v2];
-        Iterator<Edge> v2it = v2LL.iterator();
-        while(v2it.hasNext()){
-            currEdge = v2it.next();
-            if(currEdge.source == v2 & currEdge.dest == v1 ) {
-                toReturn = true;
-            }
-        }
-        return toReturn;
-    }
-
-    public void dijkstra(int source){
-        PriorityQueue<NodeCost> costMinHeap = new PriorityQueue<>();
-        int[] finalCosts = new int[numVertices];
-        for (int i=0; i<numVertices; i++){
-            if (i == source){
-                nodeCosts[source].cost = 0;
-            }
-            finalCosts[i] = MAX_VALUE;
-            costMinHeap.add(nodeCosts[i]);
-        }
-        int num_finalized = 0;
-
-        while (num_finalized < numVertices){
-            NodeCost curNode = costMinHeap.remove();
-            int curVertex = curNode.node;
-            if (finalCosts[curVertex] == MAX_VALUE){
-                finalCosts[curVertex] = curNode.cost;
-                num_finalized++;
-                Iterator<Edge> it = vertexArr[curVertex].iterator();
-                while(it.hasNext()){
-                    Edge curEdge = it.next();
-                    if ((curEdge.weight + curNode.cost) < nodeCosts[curEdge.dest].cost) {
-                        nodeCosts[curEdge.dest].cost = curEdge.weight + curNode.cost;
-                        //Note: we're adding a new NodeCost b/c Java's priority queue doesn't allow us to update values :-(
-                        NodeCost insertedNode = new NodeCost(curEdge.dest, curEdge.weight + finalCosts[curVertex]);
-                        costMinHeap.add(insertedNode);
-                    }
+            for (String neighbor : distances.get(current).keySet()) {
+                int newDistance = distancesFromStart.get(current) + distances.get(current).get(neighbor);
+                if (newDistance < distancesFromStart.get(neighbor)) {
+                    distancesFromStart.put(neighbor, newDistance);
+                    previous.put(neighbor, current);
+                    // Reorder the priority queue to reflect the updated distances
+                    queue.remove(neighbor);
+                    queue.add(neighbor);
                 }
             }
         }
-        System.out.println(Arrays.toString(finalCosts));
+
+        return Collections.emptyList();  // No path found
     }
+
+    public Map<String, HashMap<String, Integer>> getCountries() {
+        return countries;
+    }
+
+    public HashMap<String, HashMap<String, Integer>> getDistances() {
+        return distances;
+    }
+
+    public Map<String, HashMap<String, Integer>> getBorders() {
+        return borders;
+    }
+    
+    public Map<String, HashMap<String, Integer>> setCountries(HashMap<String, HashMap<String, Integer>> countries) {
+        return this.countries = countries;
+    }
+    public Map<String, HashMap<String, Integer>> setDistances(HashMap<String, HashMap<String, Integer>> distances) {
+        return this.distances = distances;
+    }
+    public Map<String, HashMap<String, Integer>> setBorders(HashMap<String, HashMap<String, Integer>> borders) {
+        return this.borders = borders;
+    }
+
+    // public void displayGraph() {
+    //     for (String country : countries.keySet()) {
+    //         System.out.println("Country " + country + ":");
+    //         System.out.println("  Neighboring countries:");
+    //         for (String neighbor : borders.get(country).keySet()) {
+    //             System.out.println("    " + neighbor);
+    //         }
+    //         System.out.println("  Distances to other countries:");
+    //         for (Map.Entry<String, Integer> entry : distances.get(country).entrySet()) {
+    //             System.out.println("    " + entry.getKey() + " (Distance: " + entry.getValue() + " km)");
+    //         }
+    //         System.out.println();
+    //     }
+    // }
 }
+
+
--- IRoadTrip.java
+++ IRoadTrip.java
@@ -10,54 +10,30 @@ import java.io.IOException;
 import java.util.regex.Pattern; 
 import java.util.regex.Matcher; 
 
+/**
+ * The IRoadTrip class represents a road trip planner that calculates the shortest path and distance between countries.
+ * It uses a graph data structure to store the countries and their distances.
+ */
 public class IRoadTrip {
+
+    private HashMap<String, HashMap<String, Integer>> country = new HashMap<String, HashMap<String, Integer>>();
+    private HashMap<String, Integer> borderCountryList = new HashMap<String, Integer>();
+    private HashMap<String, String> countryIDToCountryName = new HashMap<String, String>();   
+    private HashMap<String, HashMap<String, Integer>> countryDistance= new HashMap<String, HashMap<String, Integer>>();
+    private HashMap<String, Integer> countriesDistance = new HashMap<String, Integer>();
+
+
+    private HashMap<String, HashMap<String, Integer>> countryToCountryDistance = new HashMap<String, HashMap<String, Integer>>();
+
+    private int distance;
+    Graph graphOfCountries = new Graph(country, countryDistance, countryIDToCountryName);
 
     public IRoadTrip(String[] args) {
         // Replace with your code
-        if (args.length == 1) {
-        try (BufferedReader reader = new BufferedReader(new FileReader("borders.txt"))) {
-            HashMap<String, HashMap<String, Integer>> country = new HashMap<String, HashMap<String, Integer>>();
-            String line;
-            while ((line = reader.readLine()) != null) {
-                String[] parts = line.split("=|;");
-                String country1 = parts[0].trim();
-                HashMap<String, Integer> borderCountryList = new HashMap<String, Integer>();
-                for(int i = 1; i < parts.length; i++){
-                String borderingCountry = parts[i].trim();
-                    if (borderCountryList == null) {
-                        country.put(country1, borderCountryList);
-                    } else if(borderingCountry != null){
-                    borderCountryList.put(borderingCountry, 0);
-                    }
-                country.put(country1, borderCountryList);
-                }
-                System.out.println(country1 + " " + borderCountryList ); 
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    } else if (args.length == 2){
-        try (BufferedReader reader = new BufferedReader(new FileReader("state_name.tsv"))) {
-                    String line;
-                    while ((line = reader.readLine()) != null) {
-                        HashMap<String, String> countryIDToCountryName = new HashMap<String, String>();     
-                        Pattern pattern = Pattern.compile("\\b[\\S ]+\\b");
-                        Matcher matcher = pattern.matcher(line);
-
-                        if(matcher.find()){
-                        String key = getKey(matcher, 2);
-                        String keyValue = getKey(matcher, 2);
-                            countryIDToCountryName.put(key, keyValue);
-                            System.out.println("KeyGen: " + key);
-                            System.out.println("Value: " + countryIDToCountryName.get(key));
-                        }
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-    } else if (args.length == 0){
-            try (BufferedReader reader = new BufferedReader(new FileReader("capdist.csv"))) {
-             HashMap<String, HashMap<String, Integer>> country = new HashMap<String, HashMap<String, Integer>>();
+        if (args.length == 0) {
+        try (BufferedReader reader = new BufferedReader(new FileReader("capdist.csv"))) {
+             //HashMap<String, HashMap<String, Integer>> countryDistance= new HashMap<String, HashMap<String, Integer>>();
+             this.countryDistance= new HashMap<String, HashMap<String, Integer>>();
             String line;
             int skipFirstLine = 0;
             while ((line = reader.readLine()) != null) {
@@ -68,15 +44,69 @@ public class IRoadTrip {
                 String[] parts = line.split(",");
                 String country1 = parts[1].trim();
                 String country2 = parts[3].trim();
-                String distance = parts[4].trim();
-                HashMap<String, Integer> countriesDistance = new HashMap<String, Integer>();
-                countriesDistance.put(country2, Integer.parseInt(distance));
-                country.put(country1, countriesDistance);
+                this.countriesDistance = new HashMap<String, Integer>();
+                distance = Integer.parseInt(parts[4].trim());
+                countriesDistance.put(country2, distance);
+                countryDistance.put(country1, countriesDistance);    ////Country to Country Distance
+                
                 System.out.println(country1 + " " + countriesDistance ); 
             }
             } catch (IOException e) {
                     e.printStackTrace();
+        }
+        try (BufferedReader reader = new BufferedReader(new FileReader("state_name.tsv"))) {
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                     //   HashMap<String, String> countryIDToCountryName = new HashMap<String, String>();     
+                     this.countryIDToCountryName = new HashMap<String, String>();
+                        Pattern pattern = Pattern.compile("\\b[\\S ]+\\b");
+                        Matcher matcher = pattern.matcher(line);
+
+                        if(matcher.find()){
+                        String key = getKey(matcher, 2);
+                        String keyValue = getKey(matcher, 2);
+                            countryIDToCountryName.put(key, keyValue);         //Country ID to Country Name
+                            System.out.println("KeyGen: " + key);
+                            System.out.println("Value: " + countryIDToCountryName.get(key));
+                        }
+                    }
+            if(countryIDToCountryName.keySet().equals(countryDistance.keySet())){
+                System.out.println("They are equal");
             }
+        // for (HashMap.Entry<String, String> countryDistance : countryIDToCountryName.entrySet()) {
+        //     String key = countryDistance.getKey();
+        //     Integer value = countriesDistance.getValue();
+        //     String key2 = countryIDToCountryName.get(key);
+        //     Integer value2 = countryIDToCountryName.getValue;
+        //     if (countryIDToCountryName.containsKey(key)) {
+        //        countryToCountryDistance.put(key, country.get(key);
+        //     }
+        // }
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+        try (BufferedReader reader = new BufferedReader(new FileReader("borders.txt"))) {
+           // HashMap<String, HashMap<String, Integer>> country = new HashMap<String, HashMap<String, Integer>>();
+           this.country = new HashMap<String, HashMap<String,Integer>>();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split("=|;");
+                String country1 = parts[0].trim(); //gets first country
+                this.borderCountryList = new HashMap<String, Integer>();
+                for(int i = 1; i < parts.length; i++){
+                String borderingCountry = parts[i].trim();
+                    if (borderCountryList == null) {
+                        country.put(country1, borderCountryList);
+                    } else if(borderingCountry != null){
+                    borderCountryList.put(borderingCountry, 0);
+                    }
+                country.put(country1, borderCountryList);
+                }
+               // System.out.println(country1 + " " + borderCountryList ); 
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
     }
 
@@ -93,41 +123,66 @@ public class IRoadTrip {
         
     // }
 
-    public int getDistance (String country1, String country2) {
+    public int getDistance (String country1, String country2) { //do the conversion in here
         // Replace with your code
-        
+        int distance = countryDistance.get(country1).get(country2);
+        if(country1 != null && country2 != null){
+            return distance;
+        }     
         return -1;
     }
 
 
     public List<String> findPath (String country1, String country2) {
-        int distance = getDistance(country1, country2);
-        // Replace with your code
+        if(country1 != null && country2 != null){
+        List<String> shortestPath = graphOfCountries.dijkstra(country1, country2);
+        return shortestPath;
+        }
         return null;
     }
 
 
     public void acceptUserInput() {
-        // try (Scanner myObj = new Scanner(System.in)) {
-        //     String country1;
-        //     String country2;
+        try {
+          //  Graph g = new Graph();
+           // g.setCountries(country, );
+            // Example: Check if USA has a border with Canada
+           // graphOfCountries.addBorder("USA", "Canada");
+            //xgraphOfCountries.addBorder("USA", "Mexico");
 
-        //     System.out.println("Enter the name of the first country (type EXIT to quit):");
-        //     country1 = myObj.nextLine();
+            // Example: Read distances information from file or other sources
+            graphOfCountries.addDistance("USA", "Canada", 2000);
+            graphOfCountries.addDistance("USA", "Mexico", 1500);
 
-        //     System.out.println("Enter the name of the second country (type EXIT to quit):");
-        //     country2 = myObj.nextLine();
-        //     System.out.println("Country1 is: " + country1);
-        //     System.out.println("Country2 is: " + country2);
-        // }
-        System.out.println("IRoadTrip - skeleton");
+            // Get user input for start and end countries
+            try (Scanner scanner = new Scanner(System.in)) {
+                System.out.print("Enter the start country: ");
+                String startCountry = scanner.nextLine().trim();
+
+                System.out.print("Enter the end country: ");
+                String endCountry = scanner.nextLine().trim();
+
+                if(startCountry.equalsIgnoreCase("EXIT") || endCountry.equalsIgnoreCase("EXIT")){
+                    scanner.close();
+                    System.exit(1);
+                }
+                // Find the shortest path
+        
+                // Display the result
+                System.out.println("Shortest path from " + startCountry + " to " + endCountry + ": " + findPath(startCountry, endCountry) + " distance is ");
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 
     public static void main(String[] args) {
         IRoadTrip a3 = new IRoadTrip(args);
-        Graph g = new Graph(5);
-        System.out.println(g);
+        // Graph g = new Graph(5);
+        // System.out.println();
         // FileReader fr=null; 
         // try
         // { 
@@ -137,9 +192,23 @@ public class IRoadTrip {
         // { 
         //     System.out.println("File not found"); 
         // } 
-  
-        a3.acceptUserInput();
+      a3.acceptUserInput();
     }
 
+    public HashMap<String, HashMap<String, Integer>> getCountry() {
+        return country;
+    }
+
+    public HashMap<String, String> getCountryIDToCountryName() {
+        return countryIDToCountryName;
+    }
+
+    public HashMap<String, HashMap<String, Integer>> getCountryDistance() {
+        return countryDistance;
+    }
+
+    public HashMap<String, HashMap<String, Integer>> getCountryToCountryDistance() {
+        return countryToCountryDistance;
+    }
 }
 
